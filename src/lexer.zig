1ba33b895cde2796650e42a265d85b7f3cd2945b
Jacob Sanzone <jacobsanz1@gmail.com>
2025-02-13 16:23:27 -0800

    added parens tokens

---
+++ src/lexer.zig
@@ -0,0 +1,71 @@
+const std = @import("std");
+const Token = struct {
+    token_type: TokenType,
+    lexeme: []const u8,
+    literal: ?Literal,
+};
+
+pub const Literal = union(enum) {
+    number: f64,
+    string: []const u8,
+};
+
+pub const TokenType = enum {
+    EOF,
+    IDENTIFIER,
+    NUMBER,
+    STRING,
+    PLUS,
+    MINUS,
+    TIMES,
+    DIVIDE,
+    LPAREN,
+    RPAREN,
+};
+pub const Input = struct {
+    source: []const u8,
+    index: usize,
+    fn next(self: *Input) ?u8 {
+        if (self.index >= self.source.len) {
+            return null;
+        }
+        defer self.index += 1;
+        return self.source[self.index];
+    }
+    fn peek(self: *Input) ?u8 {
+        if (self.index >= self.source.len) {
+            return null;
+        }
+        return self.source[self.index];
+    }
+};
+pub fn makeInput(source: []const u8) Input {
+    return Input{
+        .source = source,
+        .index = 0,
+    };
+}
+
+pub fn Tokenizer(source: *Input) ![]Token {
+    var tokens = std.ArrayList(Token).init(std.heap.page_allocator);
+    while (source.next()) |c| {
+        const token = switch (c) {
+            ' ', '\t', '\n' => continue,
+            '(' => Token{ .token_type = TokenType.LPAREN, .lexeme = "(", .literal = null },
+            ')' => Token{ .token_type = TokenType.RPAREN, .lexeme = ")", .literal = null },
+            '+' => Token{ .token_type = TokenType.PLUS, .lexeme = "+", .literal = null },
+            '-' => Token{ .token_type = TokenType.MINUS, .lexeme = "-", .literal = null },
+            '*' => Token{ .token_type = TokenType.TIMES, .lexeme = "*", .literal = null },
+            '/' => Token{ .token_type = TokenType.DIVIDE, .lexeme = "/", .literal = null },
+            else => {
+                std.debug.print("Unknown character: {any}\n", .{c});
+                return error.UnknownCharacter;
+            },
+        };
+        try tokens.append(token);
+    }
+    const token = Token{ .token_type = TokenType.EOF, .lexeme = "EOF", .literal = null };
+    try tokens.append(token);
+
+    return tokens.toOwnedSlice();
+}
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-
+const lexer = @import("lexer.zig");
 pub fn main() !void {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     std.debug.print("Logs from your program will appear here!\n", .{});
@@ -25,7 +25,20 @@ pub fn main() !void {
 
     // Uncomment this block to pass the first stage
     if (file_contents.len > 0) {
-        @panic("Scanner not implemented");
+        var input = lexer.makeInput(file_contents);
+        const tokens = try lexer.Tokenizer(&input);
+        defer std.heap.page_allocator.free(tokens);
+        std.debug.print("Tokens:{any}\n", .{tokens});
+        for (tokens) |token| {
+            if (token.literal) |literal| {
+                switch (literal) {
+                    .number => |number| try std.io.getStdOut().writer().print("{?} {s} {d}\n", .{ token.token_type, token.lexeme, number }),
+                    .string => |string| try std.io.getStdOut().writer().print("{?} {s} {s}\n", .{ token.token_type, token.lexeme, string }),
+                }
+            } else {
+                try std.io.getStdOut().writer().print("{?} {s} null\n", .{ token.token_type, token.lexeme });
+            }
+        }
     } else {
         try std.io.getStdOut().writer().print("EOF  null\n", .{}); // Placeholder, remove this line when implementing the scanner
     }

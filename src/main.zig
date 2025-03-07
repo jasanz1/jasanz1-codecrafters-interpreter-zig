81a49516676031274764b7a3dbb5fa182b482ddc
Jacob Sanzone <jacobsanz1@gmail.com>
2025-03-06 16:43:00 -0800

    forgot to save

--- src/main.zig
+++ src/main.zig
@@ -44,7 +44,7 @@ pub fn main() !u8 {
         return 0;
     }
 
-    const value = eval.evalulate(&ast, std.mem.eql(u8, command, "evaluate")) catch return 70;
+    const value = eval.evalulate(&ast) catch return 70;
     if (std.mem.eql(u8, command, "evaluate")) {
         try eval.printValue(std.io.getStdOut().writer(), &value);
         return 0;
